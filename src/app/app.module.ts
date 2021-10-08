b9dc6ee0a99fd54b0d8c557ce00c29028b51b710
Hametti <arkadiusz.rajski1@gmail.com>
2021-10-08 12:38:47 +0200

    added announcement to welcome-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { ProgrammeComponent } from './programme/programme.component';
 import { LoginComponent } from './login/login.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { FooterComponent } from './footer/footer.component';
+import { AnnouncementComponent } from './announcement/announcement.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 @NgModule({
   declarations: [
@@ -17,14 +19,16 @@ import { FooterComponent } from './footer/footer.component';
     ProgrammeComponent,
     LoginComponent,
     AboutUsComponent,
-    FooterComponent
+    FooterComponent,
+    AnnouncementComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot([
       { path: 'programme', component: ProgrammeComponent },
-      { path: 'welcome', component: SliderComponent },
+      { path: 'welcome', component: MainPageComponent },
       { path: 'about-us', component: AboutUsComponent},
       { path: 'login', component: LoginComponent},
       { path: '**', redirectTo: '/welcome' }
